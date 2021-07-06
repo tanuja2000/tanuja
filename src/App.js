e2846d79f2069649a58f180430caaefffbb42e26
acharya <tanuja21alamuri@gmail.com>
2021-07-06 11:21:15 +0530

    changes for project

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my project.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/module-one/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore our project
         </a>
       </header>
     </div>
